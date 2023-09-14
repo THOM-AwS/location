770bc7681d7d05b8bcbdcf900ef633ac82c3e1c8
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-14 22:31:04 +1000

    make

--- ses.tf
+++ ses.tf
@@ -4,7 +4,7 @@ resource "aws_ses_domain_identity" "apse2_domain" {
 
 resource "aws_route53_record" "apse2_domain_verification" {
   zone_id = "Z012116528BBKQBWO9WHV" # You need to get this from your Route53 hosted zone
-  name    = "${aws_ses_domain_identity.apse2_domain.verification_token}._domainkey.apse2.com.au"
+  name    = "_amazonses.${aws_ses_domain_identity.apse2_domain.id}"
   type    = "TXT"
   ttl     = "600"
   records = [aws_ses_domain_identity.apse2_domain.verification_token]
@@ -12,4 +12,9 @@ resource "aws_route53_record" "apse2_domain_verification" {
 
 resource "aws_ses_email_identity" "noreply" {
   email = "noreply@apse2.com.au"
+}
+
+resource "aws_ses_domain_identity_verification" "verification" {
+  domain     = aws_ses_domain_identity.apse2_domain.id
+  depends_on = [aws_route53_record.apse2_domain_verification]
 }
