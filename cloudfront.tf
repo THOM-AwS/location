c200c0d0a8fddbccada1d438310aef83b7fad91b
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 18:18:19 +1000

    setup build

--- cloudfront.tf
+++ cloudfront.tf
@@ -40,7 +40,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    acm_certificate_arn      = aws_acm_certificate.subdomain.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
