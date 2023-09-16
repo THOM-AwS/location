a8c7051527dc9bddd85d07979bc774e364bfeed4
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 19:50:32 +1000

    setup build

--- r53.tf
+++ r53.tf
@@ -8,12 +8,12 @@ resource "aws_route53_record" "root_domain_a_record" {
   zone_id = data.aws_route53_zone.apse2_domain.zone_id
   name    = var.domain_name
   type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
+  records = ["8.8.8.8"]
+  # alias {
+  #   name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+  #   zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+  #   evaluate_target_health = false
+  # }
 }
 
 # SES Domain Verification Record
