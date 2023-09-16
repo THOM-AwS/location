f587adb014efe75e4370e07d8c2f1f9a94f88a41
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 18:13:54 +1000

    setup build

--- acm.tf
+++ acm.tf
@@ -1,5 +1,5 @@
 # Request an ACM Certificate
-resource "aws_acm_certificate" "cert" {
+resource "aws_acm_certificate" "subdomain" {
   domain_name       = "${var.subdomain_name}.${var.domain_name}"
   validation_method = "DNS"
   lifecycle {
@@ -7,10 +7,20 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_acm_certificate" "cert_base" {
+resource "aws_acm_certificate" "domain" {
   domain_name       = "*.${var.domain_name}"
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_acm_certificate_validation" "subdomain" {
+  certificate_arn         = aws_acm_certificate.subdomain.arn
+  validation_record_fqdns = [for r in aws_route53_record.subdomain : r.fqdn]
+}
+
+resource "aws_acm_certificate_validation" "domain" {
+  certificate_arn         = aws_acm_certificate.domain.arn
+  validation_record_fqdns = [for r in aws_route53_record.domain : r.fqdn]
 }
