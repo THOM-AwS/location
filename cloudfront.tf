a10ff0450b7f3df3b065f73d3f0e75e1a49bbeff
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 20:21:59 +1000

    setup build

--- cloudfront.tf
+++ cloudfront.tf
@@ -48,7 +48,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   # Add your custom domain as an alternate domain name
   aliases = [
     "${var.subdomain_name}.${var.domain_name}",
-    var.var.domain_name
+    var.domain_name
   ]
 
   price_class = "PriceClass_100"
