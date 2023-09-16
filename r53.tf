112427d81fd075b82498143b8a147cf1f4802f77
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 10:33:14 +1000

    make

--- r53.tf
+++ r53.tf
@@ -22,6 +22,23 @@ resource "aws_route53_record" "subdomain" {
   }
 }
 
+# DNS record for ACM certificate validation
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
+}
+
 resource "aws_route53_record" "cert_validation" {
   zone_id = var.route53_zone_id
   name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
@@ -33,5 +50,5 @@ resource "aws_route53_record" "cert_validation" {
 # Certificate validation
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
 }
