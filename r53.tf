ec70f6aebe8f61a45a63bb0d2bd613a43f90f56a
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 10:35:55 +1000

    make

--- r53.tf
+++ r53.tf
@@ -39,14 +39,6 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 
-resource "aws_route53_record" "cert_validation" {
-  zone_id = var.route53_zone_id
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
-  ttl     = 60
-}
-
 # Certificate validation
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
