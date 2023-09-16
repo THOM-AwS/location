65813e8bd586e3427ea2353d4c6c45c64c925892
Thomas Hamer <thomas_hamer@outlook.com>
2023-09-16 18:31:09 +1000

    setup build

--- acm.tf
+++ acm.tf
@@ -8,7 +8,7 @@ resource "aws_acm_certificate" "subdomain" {
 }
 
 resource "aws_acm_certificate" "domain" {
-  domain_name       = "*.${var.domain_name}"
+  domain_name       = var.domain_name
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
